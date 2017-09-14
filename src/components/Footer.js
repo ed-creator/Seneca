b196e39d63ae264e5c8f644043b9da6f6793716e
Edward Ward <edwardwardward@gmail.com>
2017-09-14 16:20:19 +0100

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+// jshint ignore: start
+
+import React, {Component} from 'react';
+
+class Footer extends Component {
+
+  render() {
+    return (
+      <div className='Rectangle-6'>
+        
+      </div>
+    );
+  }
+}
--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -3,3 +3,11 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Header from './Header';
+
+describe('Header',() => {
+  let header = mount(<Header />);
+
+  it('renders the header', () => {
+    expect(header.find('p.navbar-element').at(0).text()).toEqual('Home')
+  });
+});
